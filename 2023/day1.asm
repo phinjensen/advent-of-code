ab7e84516f8142c9601cb1692aa340a513bf0879
Phineas Jensen <phin@zayda.net>
2024-01-17 10:36:56 -0700

    Cleanup with better comments

--- 2023/day1.asm
+++ 2023/day1.asm
@@ -1,91 +1,85 @@
 .text
 .globl _start
 _start:
-    movq $input, %r8  # Current string address
-    movq $0, %r15     # Result value
-    jmp reset_numbers
+    movq $input, %r8  # Address of current character of input string is stored in %r8
+    movq $0, %r15     # Result value is stored in %r15
+    jmp reset_numbers # Jump to pre-loop register reset, to initialize first & last digits and the first-digit-found flag
 
 loop:
-    # Skip character if less than ascii '0'
-    cmpb $0x30, (%r8)
+    cmpb $0x30, (%r8) # Skip character if less than ascii '0'
     jl inc
 
-    # Skip character if greater than ascii '9'
-    cmpb $0x39, (%r8)
+    cmpb $0x39, (%r8) # Skip character if greater than ascii '9'
     jg inc
 
-    movb (%r8), %r10b
-    subb $0x30, %r10b
+    movb (%r8), %r10b # Copy current character into r10
+    subb $0x30, %r10b # convert from ascii to plain integer
 
-    # If first digit hasn't been found, update both
-    cmpb $0x0, %r13b
-    jne last_number
+    cmpb $0x0, %r13b  # If the first digit has been found
+    jne last_digit    # jump directly to updating the last digit
 
-    movb %r10b, %r11b
-    incb %r13b
+    movb %r10b, %r11b # Update the first number for the line (%r11b)
+    incb %r13b        # and mark the first number as found (%r13b)
 
-last_number:
-    movb %r10b, %r12b
+last_digit:
+    movb %r10b, %r12b    # Update the last digit found
 
 inc:
-    # TODO: next step is to check for a newline when incrementing,
-    # if the value is a newline, then multiply first digit by 10,
-    # add to result, then add second digit to result. Then reset numbers.
-    incq %r8
-    cmpq $input_end, %r8
-    jge print_result
+    incq %r8             # Move character pointer
+    cmpq $input_end, %r8 # Check if we're at the end of the input
+    jge print_result     # and jump to printing the result if so
 
-    cmpb $0x0a, (%r8)
-    je add_to_result
+    cmpb $0x0a, (%r8)    # Check if the current character is a newline,
+    je add_to_result     # and add to final result if so
 
-    jmp loop
+    jmp loop             # Jump to beginning of the main loop
 
 reset_numbers:
     movb $0, %r11b # first digit
     movb $0, %r12b # last digit
     movb $0, %r13b # first digit found flag
-    jmp loop
+    jmp loop       # jump to the beginning of the main loop
 
 add_to_result:
-    imulq $10, %r11
-    addq %r11, %r15
-    addq %r12, %r15
-    incq %r8
-    cmpq $input_end, %r8
-    jl reset_numbers
+    imulq $10, %r11      # Multiply first digit by 10
+    addq %r11, %r15      # Add first digit to result
+    addq %r12, %r15      # Add last digit to result
+    incq %r8             # Move character pointer forward
+    cmpq $input_end, %r8 # Check if we're at the end of the input
+    jl reset_numbers     # and jump to printing the result if so
 
 print_result:
-    movq $result_end, %r14 # end of output area
+    movq $result_end, %r14 # Keep output pointer (end of output location) in %r14
 
-    movq %r15, %rax
-    movq $10, %rcx
+    movq %r15, %rax  # Store result (dividend) in %rax
+    movq $10, %rcx   # Store divisor in %rcx
 
 print_loop:
-    divq %rcx
+    movq $0, %rdx    # Reset first 64 bits of dividend to 0
+    divq %rcx        # Divide!
 
-    addq $0x30, %rdx
-    movb %dl, (%r14)
+    addq $0x30, %rdx # Remainder is stored in %rdx, so add 0x30 to make it ascii
+    movb %dl, (%r14) # Put character at output pointer location
 
-    cmpq $0, %rax
-    je print
+    cmpq $0, %rax    # If the result of division was 0
+    je print         # jump to print the result
 
-    decq %r14
-    movq $0, %rdx
-    jmp print_loop
+    decq %r14        # Move character pointer left
+    jmp print_loop   # Go back to the start of the loop
 
 print:
-    movq $0x04, %rax
-    movq $0x1, %rbx
-    movq %r14, %rcx
-    movq $result_end, %rdx
-    sub %r14, %rdx
-    incq %rdx
-    int $0x80
+    movq $0x04, %rax       # 4 is write()
+    movq $0x1, %rbx        # File descriptor 1 (stdout)
+    movq %r14, %rcx        # Pointer to result string
+    movq $result_end, %rdx # Store address of result string as output length
+    sub %r14, %rdx         # and subtract the beginning of the string
+    incq %rdx              # and add 1 to get the actual length of the string
+    int $0x80              # Send the syscall interrupt
 
 end:
-    movl $0x01, %eax
-    movl %r15d, %ebx
-    int $0x80
+    movl $1, %eax # 1 is exit()
+    movl $0, %ebx # Exit status 0
+    int $0x80     # Send the syscall interrupt
 
 .data
 result: .ascii "         "
