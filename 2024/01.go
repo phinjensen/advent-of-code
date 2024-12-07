8f4f62b209f766ca0cb447095694198dc95dcd03
Phineas Jensen <phin@zayda.net>
2024-12-07 00:52:09 -0700

    Consolidate day 1

--- 2024/01.go
+++ 2024/01.go
@@ -6,6 +6,28 @@ import (
 	"strconv"
 )
 
+func Day1_1() {
+	var a, b string
+	var left, right []int
+	for {
+		_, err := fmt.Scanln(&a, &b)
+		if err != nil {
+			break
+		}
+		a_int, _ := strconv.Atoi(a)
+		b_int, _ := strconv.Atoi(b)
+		left = append(left, a_int)
+		right = append(right, b_int)
+	}
+	slices.Sort(left)
+	slices.Sort(right)
+	result := 0
+	for i := range len(left) {
+		result += Abs(left[i] - right[i])
+	}
+	fmt.Println(result)
+}
+
 func Day1_2() {
 	var a, b string
 	var left, right []int
